3e419943a04976a16d947b83bff7d47c62fbc6f5
Charlie Waite <cw5790@gmail.com>
2018-05-14 19:59:07 +0100

    Codewars: Highest and Lowest

---
+++ Highest and Lowest/highestLowest.js
@@ -0,0 +1,7 @@
+function highAndLow(numbers){
+    const numbersArray = numbers.split(' ').map(Number)
+
+    return `${Math.max(...numbersArray)} ${Math.min(...numbersArray)}`
+}
+
+module.exports = highAndLow;
---
+++ Highest and Lowest/highestLowest.test.js
@@ -0,0 +1,33 @@
+const chai = require('chai'), expect = chai.expect, should = chai.should;
+const highAndLow = require('./highestLowest');
+
+const testData = [
+    {
+        values: '4 11 14 9 10 100 44 3 5 12',
+        expected: '100 3',
+    },
+    {
+        values: '4 5 29 54 4 0 -214 542 -64 1 -3 6 -6',
+        expected: '542 -214',
+    },
+    {
+        values: '1 -1',
+        expected: '1 -1',
+    },
+    {
+        values: '44 55 33 22 88 99 77 66',
+        expected: '99 22',
+    },
+    {
+        values: '1 2 3 4 7 5 6',
+        expected: '7 1',
+    },
+];
+
+describe('High And Low', () => {
+    testData.forEach(element => {
+        it(`should display ${element.expected} when the input is ${element.values}`, () => {
+            expect(element.expected).to.equal(highAndLow(element.values));
+        });
+    });
+});
